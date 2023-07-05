a58778b2f30f8f571d5c6fbe1f39c46465fe597b
Samuel Arandia <sarandia@advise.cl>
2023-07-05 12:04:17 -0400

    config cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-
+  app.enableCors();
+  
+  
   app.useGlobalPipes( 
     new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     })
-  );
+    );
+  
+  await app.listen(3000);
 }
 bootstrap();
